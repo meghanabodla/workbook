9e43507ed6e5044a70dd69dbdc5a6d71a5553731
meghanabodla <meghanabodla07@gmail.com>
2026-01-23 08:22:54 +0530

    23-01-2026

---
+++ Skill-5/src/main/java/com/klu/model/Student.java
@@ -0,0 +1,28 @@
+package com.klu.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    private int id;
+    private String name;
+    private String gender;
+
+    @Autowired
+    private Certification certification;
+
+    public Student() {
+        this.id = 167;
+        this.name = "Meghana";
+        this.gender = "Female";
+    }
+
+    public void displayStudentDetails() {
+        System.out.println("Student ID   : " + id);
+        System.out.println("Student Name : " + name);
+        System.out.println("Gender       : " + gender);
+        System.out.println("Certification: " + certification);
+    }
+}
